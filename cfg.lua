822171adb5d515a7ba88e2743d451d4e3e7bf747
Slowmmo <73839937+Namenoy@users.noreply.github.com>
2025-08-14 14:42:14 +0800

    Create cfg.lua

---
+++ cfg.lua
@@ -0,0 +1,17 @@
+ScoreBoardCFG = ScoreBoardCFG or {} 
+
+ScoreBoardCFG.jobs = {
+    category = {
+        {
+            name = "Citizen", -- JOB CATEGORY
+            color = Color(241,113,113), -- JOB COLOR
+            jobs = { TEAM_SASA, TEAM_SOSA } -- JOBS LIST
+        },
+
+        {
+            name = "Mafia",
+            color = Color(255,255,255),
+            jobs = { TEAM_LALA, TEAM_LOLA } 
+        }
+    }
+}
